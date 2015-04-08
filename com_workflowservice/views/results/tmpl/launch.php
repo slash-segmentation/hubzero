3bc70a937fa2ca2d7c21c2df8d65304d85fcfc69
Mike Chiu <michiu@ucsd.edu>
2015-04-08 15:45:29 -0700

    Add link to wiki documentation

--- com_workflowservice/views/results/tmpl/launch.php
+++ com_workflowservice/views/results/tmpl/launch.php
@@ -30,6 +30,7 @@ defined('_JEXEC') or die( 'Restricted access' );
 		<div id="boxo">
 			<div id="lefto">
 				<div><?php echo nl2br($this->workflow_description); ?></div>
+				<div><p>Read <a href="/wiki/<?php echo str_replace(" ", "", $this->workflow_name); ?>" target="_blank">documentation</a></p></div>
 
 				<form name="myform" id="myform">
 
